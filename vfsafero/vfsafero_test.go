0b3a2d5ba2108377f73333357d2c15c934557e9f
Tom Payne <twpayne@gmail.com>
2018-12-07 11:01:31 +0100

    Add afero compatibility test

--- vfsafero/vfsafero_test.go
+++ vfsafero/vfsafero_test.go
@@ -1,7 +1,31 @@
 package vfsafero
 
-import "github.com/spf13/afero"
+import (
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/twpayne/go-vfs/vfst"
+)
 
 var (
 	_ afero.Fs = &AferoFS{}
 )
+
+func TestAferoFS(t *testing.T) {
+	fs, cleanup, err := vfst.NewTestFS(map[string]interface{}{
+		"/home/user/.bashrc": "# contents of .bashrc\n",
+	})
+	defer cleanup()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	aferoFS := NewAferoFS(fs)
+	afero.WriteFile(aferoFS, "/home/user/foo", []byte("bar"), 0666)
+
+	vfst.RunTests(t, fs, "",
+		vfst.TestPath("/home/user/foo",
+			vfst.TestContentsString("bar"),
+		),
+	)
+}
