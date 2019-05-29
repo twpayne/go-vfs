f764e12db75e538d4df2228834476f11e2ac58ca
Tom Payne <twpayne@gmail.com>
2019-05-29 20:42:57 +0200

    Tidy up path handling

--- posix.go
+++ posix.go
@@ -7,6 +7,11 @@ import "syscall"
 // HostOSFS is the host-specific OSFS.
 var HostOSFS = OSFS
 
+// relativizePath, on POSIX systems, just returns path.
+func relativizePath(path string) string {
+	return path
+}
+
 func shouldSkipSystemError(err syscall.Errno) bool {
 	return false
 }
--- windows.go
+++ windows.go
@@ -3,6 +3,7 @@
 package vfs
 
 import (
+	"path/filepath"
 	"syscall"
 
 	"golang.org/x/sys/windows"
@@ -10,6 +11,15 @@ import (
 
 // HostOSFS is the host-specific OSFS.
 var HostOSFS = WindowsOSFS{}
+
+// relativizePath, on Windows, strips any leading volume name from path and
+// replaces backslashes with slashes.
+func relativizePath(path string) string {
+	if volumeName := filepath.VolumeName(path); volumeName != "" {
+		path = path[len(volumeName):]
+	}
+	return filepath.ToSlash(path)
+}
 
 func shouldSkipSystemError(err syscall.Errno) bool {
 	return err == windows.ERROR_CANT_RESOLVE_FILENAME
