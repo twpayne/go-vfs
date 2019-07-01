48f70e2cccb814449bb4f8cae3472aa8fe9026d7
Tom Payne <twpayne@gmail.com>
2019-07-01 19:18:25 +0200

    Wrap comments

--- windows.go
+++ windows.go
@@ -22,9 +22,9 @@ var ignoreErrnoInContains = map[syscall.Errno]struct{}{
 	windows.ERROR_CANT_RESOLVE_FILENAME: {},
 }
 
-// relativizePath, on Windows, strips any leading volume name from path.
-// since this is used to prepare paths to have the prefix prepended,
-// returned values use slashes instead of backslashes.
+// relativizePath, on Windows, strips any leading volume name from path. Since
+// this is used to prepare paths to have the prefix prepended, returned values
+// use slashes instead of backslashes.
 func relativizePath(path string) string {
 	if volumeName := filepath.VolumeName(path); volumeName != "" {
 		path = path[len(volumeName):]
@@ -33,9 +33,9 @@ func relativizePath(path string) string {
 }
 
 // trimPrefix, on Windows, trims prefix from path and returns an absolute path.
-// prefix must be a /-separated path.
-// since this is used to prepare results to be returned to the calling client,
-// returned values use backslashes instead of slashes
+// prefix must be a /-separated path. Since this is used to prepare results to
+// be returned to the calling client, returned values use backslashes instead of
+// slashes
 func trimPrefix(path, prefix string) (string, error) {
 	trimmedPath, err := filepath.Abs(strings.TrimPrefix(filepath.ToSlash(path), prefix))
 	if err != nil {
