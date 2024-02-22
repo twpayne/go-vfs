abfa11ba18404a82c886138c7f6a5587e514596c
Tom Payne <twpayne@gmail.com>
2024-02-22 20:52:47 +0100

    Ensure walkFn is called for all errors

--- vfst/fs_test.go
+++ vfst/fs_test.go
@@ -1,8 +1,10 @@
 package vfst_test
 
 import (
+	"errors"
 	"io/fs"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
@@ -37,4 +39,92 @@ func TestWalk(t *testing.T) {
 		"/home/user/symlink": fs.ModeSymlink,
 	}
 	assert.Equal(t, expectedPathTypeMap, pathTypeMap)
+}
+
+func TestWalkErrors(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("test uses UNIX file permissions")
+	}
+	for _, tc := range []struct {
+		name          string
+		root          any
+		postFunc      func(vfs.FS) error
+		expectedPaths []string
+	}{
+		{
+			name: "empty",
+			expectedPaths: []string{
+				"/",
+			},
+		},
+		{
+			name: "simple",
+			root: map[string]any{
+				"/dir/subdir/subsubdir/file": "",
+			},
+			expectedPaths: []string{
+				"/",
+				"/dir",
+				"/dir/subdir",
+				"/dir/subdir/subsubdir",
+				"/dir/subdir/subsubdir/file",
+			},
+		},
+		{
+			name: "private_subdir",
+			root: map[string]any{
+				"/dir/subdir/subsubdir/file": "",
+			},
+			postFunc: func(fileSystem vfs.FS) error {
+				return fileSystem.Chmod("/dir/subdir", 0)
+			},
+			expectedPaths: []string{
+				"/",
+				"/dir",
+				"/dir/subdir",
+			},
+		},
+		{
+			name: "private_subdir_keep_going",
+			root: map[string]any{
+				"/dir/subdir/subsubdir/file": "",
+				"/dir/subdir2/file":          "",
+			},
+			postFunc: func(fileSystem vfs.FS) error {
+				return fileSystem.Chmod("/dir/subdir", 0)
+			},
+			expectedPaths: []string{
+				"/",
+				"/dir",
+				"/dir/subdir",
+				"/dir/subdir2",
+				"/dir/subdir2/file",
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			fileSystem, cleanup, err := vfst.NewTestFS(tc.root)
+			assert.NoError(t, err)
+			if tc.postFunc != nil {
+				assert.NoError(t, tc.postFunc(fileSystem))
+			}
+			defer cleanup()
+			var actualPaths []string
+			assert.NoError(t, vfs.Walk(fileSystem, "/", func(path string, info fs.FileInfo, err error) error {
+				switch {
+				case errors.Is(err, fs.ErrPermission):
+					if info.IsDir() {
+						return vfs.SkipDir
+					}
+					return nil
+				case err != nil:
+					return err
+				default:
+					actualPaths = append(actualPaths, path)
+					return nil
+				}
+			}))
+			assert.Equal(t, tc.expectedPaths, actualPaths)
+		})
+	}
 }
